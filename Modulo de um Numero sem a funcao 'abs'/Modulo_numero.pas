b2a9483ea43a0846475e6c6c4e0c3277db9d11ee
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-08-23 07:36:20 +0200

    Create Modulo_numero.pas

---
+++ Modulo de um Numero sem a funcao 'abs'/Modulo_numero.pas
@@ -0,0 +1,32 @@
+// -------------------------------------------------------------------------------
+// Escreva um programa Pascal para determinar o módulo de um número. Recordemo-nos de 
+// que 
+//
+// modulo(X) =
+// X se X >= 0
+// -X se X < 0
+//
+// NB: Não utilize a função pré-definida (embutida) do Turbo Pascal abs(x).
+// -------------------------------------------------------------------------------
+// Autor			: Fernando Gomes
+// Data			: 22/08/2021
+// -------------------------------------------------------------------------------
+
+
+Program Modulo_numero ;
+var
+	x, modulo: real;
+Begin
+	write('Digite o número: ');
+	readln(x);
+	
+	if (x > 0) then
+		write('O módulo de ',x:5:2, ' e ',x:5:2)
+	else
+		begin
+			modulo := x * -1;
+			write('O módulo de ',x:5:2, ' e ',modulo:5:2);	
+		end
+	
+  
+End.
